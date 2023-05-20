5a20a6c87d11d8364e70d6afa92f7fe16240eb37
koplenov <koplenov@gmail.com>
2023-05-20 20:13:59 +0300

    move to $mol_book2_catalog

--- marker/infinity.view.ts
+++ marker/infinity.view.ts
@@ -4,20 +4,20 @@ namespace $.$$ {
 		src: string
 	}
 
-	export class $koplenov_infinity extends $.$koplenov_infinity {
+	export class $koplenov_infinity_marker extends $.$koplenov_infinity_marker {
 		// отображаем Row
 		@$mol_mem_key
 		Pics( index: any ) {
-			// let page_size = this.chunk_size()
-			// let page = Math.floor( index / page_size )
-			return this.links( index ).reverse().map( data => this.Pic( data ) )// [this.Pic(this.links( page )[ index % page_size ])]
+			return this.links( index ).reverse().map( data => this.Pic( data ) )
 		}
+
 		// получаем страницу данных
 		@$mol_mem_key
 		links( page: number ) {
 			const uri = `https://ai.img-converter.com/report/${ this.Formats().value() }?page=${ page || 1 }&count=${ this.chunk_size() }&query=`
 			return $mol_fetch.json( uri ) as Record[]
 		}
+
 		// ээ, шото необходимое для работы подзагрузки
 		@$mol_mem_key
 		after( anchor_id: number ) {
@@ -29,7 +29,7 @@ namespace $.$$ {
 
 		@$mol_mem_key
 		image( data: Record ) {
-			$mol_wire_solid()
+			// $mol_wire_solid()
 			return data.src
 		}
 	}
