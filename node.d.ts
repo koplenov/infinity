7bb061be6b952158f794730fa962590474e32b44
koplenov <koplenov@users.noreply.github.com>
2023-10-02 07:09:27 +0000

    Deploying to gh-pages from @ koplenov/infinity@d72f1f49815a7a2521c0a7da4ad6a43bcd2685f5 🚀

--- node.d.ts
+++ node.d.ts
@@ -2400,6 +2400,7 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_check_list extends $mol_view {
+        dictionary(): Record<string, any>;
         Option(id: any): $$.$mol_check;
         options(): Record<string, any>;
         keys(): readonly string[];
@@ -2419,6 +2420,8 @@ declare namespace $.$$ {
         options(): {
             [key: string]: string;
         };
+        dictionary(next?: Record<string, boolean>): Record<string, boolean>;
+        option_checked(id: string, next?: boolean | null): boolean;
         keys(): readonly string[];
         items(): $mol_check[];
         option_title(key: string): string;
