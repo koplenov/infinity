93509c6b428e75c94f4e673186e5fb3f5e1dffd3
koplenov <koplenov@users.noreply.github.com>
2023-10-10 07:09:31 +0000

    Deploying to gh-pages from @ koplenov/infinity@d72f1f49815a7a2521c0a7da4ad6a43bcd2685f5 🚀

--- node.js
+++ node.js
@@ -3459,6 +3459,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
--- node.test.js
+++ node.test.js
@@ -3451,6 +3451,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
