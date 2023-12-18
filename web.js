cb7f02e3a246ba9f6ebeb5c3f9ac854604eea3f9
koplenov <koplenov@users.noreply.github.com>
2023-12-18 07:09:35 +0000

    Deploying to gh-pages from @ koplenov/infinity@d72f1f49815a7a2521c0a7da4ad6a43bcd2685f5 🚀

--- web.js
+++ web.js
@@ -3017,7 +3017,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -3051,6 +3052,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
